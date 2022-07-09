7e11ccb60762fbb1a97fa6b65de9af88b9979faa
Jibin Joy <77064989+code-lover-jarvis@users.noreply.github.com>
2022-07-09 22:02:41 +0530

    Added comparison, exchange, inner loop, outer loop

--- B_Sort.c
+++ B_Sort.c
@@ -4,11 +4,11 @@
 
 int main()
 {
-   int a[20], n, i, j, cho, temp;
+   int a[20], n, i, j, cho, temp, xchange_count = 0, comparison_count = 0, inner_loop = 0, outer_loop = 0;
    system("cls");
    printf("Enter the number of Elemnts : ");
    scanf("%d", &n);
-   printf("Enter the Elemnts : ");
+   printf("Enter the Elements : ");
    for(i = 0; i < n; i++)
    {
     scanf("%d", &a[i]);
@@ -17,24 +17,30 @@ int main()
    scanf("%d", &cho);
    for(i = 0; i < n - 1; i++)
    {
+    ++outer_loop;
     for(j = 0; j < n - i - 1; j++)
     {
+        ++inner_loop;
         if(cho == 1)
         {
             if( a[j] > a[j + 1])
             {
+                ++comparison_count;
                 temp = a[j];
                 a[j] = a[j+1];
                 a[j+1] = temp;
+                xchange_count += 3;
             }
         }
         else
         {
             if( a[j] < a[j + 1])
             {
+                ++comparison_count;
                 temp = a[j];
                 a[j] = a[j+1];
                 a[j+1] = temp;
+                xchange_count += 3;
             }
         }
     }
@@ -44,6 +50,7 @@ int main()
    {
     printf("%d\t", a[i]);
    }
+   printf("\nNumber of Exchange Operation : %d\nNumber of Comparison Operation : %d\nInner Loop : %d\nOuter Loop : %d", xchange_count, comparison_count, inner_loop, outer_loop);
    getch();
    return(0);
 }
